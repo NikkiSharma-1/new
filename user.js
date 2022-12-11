86204ea282c2f092ff73d7729c683dff80e1d009
NikkiSharma-1 <114333988+NikkiSharma-1@users.noreply.github.com>
2022-12-11 20:59:51 +0530

    Add files via upload

--- user.js
+++ user.js
@@ -1,3 +1,39 @@
+const db=require('./database');
+const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
+
+function isstringinvalid(string){
+    if(string == undefined || string.length === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const signup= async (req,res) => {
+    try {
+        const {name ,email,password} =req.body;
+        console.log('email' ,email);
+        if(isstringinvalid(name) || isstringinvalid(email) ||isstringinvalid(password)){
+            return res.status(500).json({err: 'Bad parameters something is missing'});
+        }
+        const saltrounds=10;
+        bcrypt.hash(password ,saltrounds ,async(err,hash) =>{
+            console.log(err);
+            await User.create({name ,email,password:hash});
+            res.status(202).json({message :'User created successfully'})
+        })
+    }catch(err){
+        res.status(505).json(err);
+    }
+}
+
+function generateToken(id){
+    return jwt.sign({userId:id} ,'8325521hdjbscs6e9012ddbssdmvlfha63i032mjesadhvkdvhydhaa');
+}
+
+
 const login=(req,res) =>{
     const {email, password}=req.body;
     if(isstringinvalid(email) || isstringinvalid(password)){
@@ -11,7 +47,7 @@ const login=(req,res) =>{
                 res.status(500).json({success:false, message:"Something went wrong"});
             }
             if(result ===true){
-                res.status(200).json({success :true ,message :"User logged in Successfully"});
+                res.status(200).json({success :true ,message :"User logged in Successfully" ,token:generateToken(user[0].id)});
             }
             else{
                 return res.status(400).json({success :false ,message :"Password is incorrect"});
@@ -24,3 +60,6 @@ const login=(req,res) =>{
         res.status(500).json({message :"Error" ,success:false});
     })
 }
+
+module.exports=signup;
+module.exports=login;
